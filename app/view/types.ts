db22313abc0505edce66232adab9e8ba29460028
Fiyifoluwa <fiyifoluwa@outlook.com>
2022-11-06 15:37:47 +0100

    navigation screen names declaration

--- app/view/types.ts
+++ app/view/types.ts
@@ -2,6 +2,8 @@ import { StackScreenProps } from '@react-navigation/stack';
 
 export type AppStackParamList = {
   Walkthrough: undefined;
+  MainScreen: undefined;
+  ItemScreen: undefined;
 };
 
 export type AppStackScreenProps<T extends keyof AppStackParamList> =

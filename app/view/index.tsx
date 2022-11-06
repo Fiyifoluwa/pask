f3b134d22ff52ce6ea9d07ba4b806b0d1d713382
Fiyifoluwa <fiyifoluwa@outlook.com>
2022-11-06 15:44:35 +0100

    add main and item screens to stack

--- app/view/index.tsx
+++ app/view/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Walkthrough from './Walkthrough';
+import MainScreen from './MainScreen';
+import ItemScreen from './ItemScreen';
 
 const Stack = createStackNavigator();
 
@@ -8,6 +10,8 @@ const AppStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Walkthrough" component={Walkthrough} />
+      <Stack.Screen name="MainScreen" component={MainScreen} />
+      <Stack.Screen name="ItemScreen" component={ItemScreen} />
     </Stack.Navigator>
   );
 };

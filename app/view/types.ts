e7409d1b3750880a1814c7a4fb6a55f3d23b6bd6
Fiyifoluwa <fiyifoluwa@outlook.com>
2022-11-08 10:09:43 +0100

    add itemscreen type to stack types

--- app/view/types.ts
+++ app/view/types.ts
@@ -1,9 +1,10 @@
 import { StackScreenProps } from '@react-navigation/stack';
+import { TCategoryItem } from 'app/types';
 
 export type AppStackParamList = {
   Walkthrough: undefined;
   MainScreen: undefined;
-  ItemScreen: undefined;
+  ItemScreen: { item: TCategoryItem };
 };
 
 export type AppStackScreenProps<T extends keyof AppStackParamList> =

b141d9861ebe9f3e967c4242970c336b9a5b6dc5
Fiyifoluwa <fiyifoluwa@outlook.com>
2022-11-08 10:11:10 +0100

    cleanup mainscreen

--- app/view/MainScreen/index.tsx
+++ app/view/MainScreen/index.tsx
@@ -1,163 +1,161 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import {
-  CategoryItem,
+  CategoryItem as CatItem,
   Container,
+  EmptyState,
   Input,
   MainCart,
   MealItem,
   ScrollViewH,
 } from 'app/components';
 import { H } from 'app/design/typography';
-import {
-  ActivityIndicator,
-  ScrollView,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import { Image, ScrollView, TouchableOpacity, View } from 'react-native';
 import GlobalStyles, { colors } from 'app/design/GlobalStyles';
 import { AppState } from 'app/redux/types';
 import { connect } from 'react-redux';
-import { getCategoryItemsDispatcher } from 'app/redux/cart/dispatchers';
-import { screenWidth } from 'app/design/responsiveModule';
+import {
+  addItemToCartDispatcher,
+  getCategoryItemsDispatcher,
+} from 'app/redux/cart/dispatchers';
+import { styles } from './styles';
+import { TCartItem, TCategory, TCategoryItem } from 'app/types';
 
 interface IMainScreen extends INavigationProps {
   getCategoryItems: (category: string) => void;
-  categories: any;
-  categoryItems: any;
+  addItemToCart: (item: TCategoryItem) => void;
+  categories: TCategory[] | null;
+  categoryItems: TCategoryItem[] | null;
+  cart: TCartItem[] | null;
 }
 
 const MainScreen = ({
   navigation,
   getCategoryItems,
+  addItemToCart,
   categories,
   categoryItems,
+  cart,
 }: IMainScreen) => {
-  const [searchInput, setSearchInput] = useState<string>();
-  const [searchCategory, setSearchCategory] = useState<string>(
-    categories[0].strCategory,
-  );
+  const [searchInput, setSearchInput] = useState<string>('');
+  const [searchCategory, setSearchCategory] = useState<string>('Beef');
 
   useEffect(() => {
     getCategoryItems(searchCategory);
   }, [searchCategory]);
 
+  const newCategories = useMemo(
+    () =>
+      categoryItems?.filter(
+        d =>
+          searchInput === '' ||
+          d.strMeal.toLowerCase().includes(searchInput?.toLowerCase()),
+      ),
+    [categoryItems, searchInput],
+  );
+
+  console.log('new', newCategories);
+
   return (
     <Container>
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        bounces={false}
-        style={{ flex: 1, position: 'relative' }}
-      >
-        <View
-          style={[
-            GlobalStyles.wrapper,
-            {
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-              marginBottom: 20,
-            },
-          ]}
+      <View style={{ flex: 1, position: 'relative' }}>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          bounces={false}
+          style={styles.mainScrollView}
         >
-          <View>
-            <H
-              text={'Hi Fiyi'}
-              fontSize={20}
-              style={{ opacity: 0.5, marginBottom: 12 }}
-            />
-            <H text={'Hungry Now? 🔥'} fontSize={30} style={{}} />
-          </View>
-          <View
-            style={{
-              borderRadius: 99,
-              backgroundColor: colors.primaryColor,
-              height: 50,
-              width: 50,
-              alignSelf: 'flex-start',
-            }}
-          />
-        </View>
-        <View
-          style={[
-            GlobalStyles.wrapper,
-            {
-              marginBottom: 20,
-            },
-          ]}
-        >
-          <Input
-            placeholder={'Search...'}
-            value={searchInput}
-            onChangeText={text => setSearchInput(text)}
-          />
-        </View>
-
-        <ScrollViewH>
-          {categories.map((item: any) => (
-            <TouchableOpacity
-              key={item.idCategory}
-              onPress={() => {
-                setSearchCategory(item.strCategory);
-              }}
-              activeOpacity={0.8}
-            >
-              <CategoryItem
-                item={item}
-                selected={searchCategory === item.strCategory}
+          <View style={styles.headerView}>
+            <View>
+              <H text={'Hi Fiyi'} fontSize={20} style={styles.nameText} />
+              <H
+                text={'Hungry Now? 🔥'}
+                fontSize={30}
+                style={{ color: colors.black }}
               />
-            </TouchableOpacity>
-          ))}
-        </ScrollViewH>
-
-        <View
-          style={[
-            GlobalStyles.wrapper,
-            {
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-              marginBottom: 20,
-            },
-          ]}
-        >
-          <H text={'Popular Items'} fontSize={24} style={{}} />
-          <H text={'See all'} fontSize={16} style={{ opacity: 0.5 }} />
-        </View>
-        <ScrollViewH>
-          {categoryItems.length ? (
-            categoryItems.map((item: any) => (
-              <TouchableOpacity
-                key={item.idMeal}
-                onPress={() => navigation?.navigate('ItemScreen', { item })}
-              >
-                <MealItem item={item} />
-              </TouchableOpacity>
-            ))
-          ) : (
-            <View
-              style={{
-                alignItems: 'center',
-                justifyContent: 'center',
-                alignSelf: 'center',
-                width: screenWidth - 40,
-                flex: 1,
-              }}
-            >
-              <ActivityIndicator size={'large'} />
             </View>
-          )}
-        </ScrollViewH>
 
-        <View>
-          <MainCart />
+            <Image
+              source={{ uri: 'https://randomuser.me/api/portraits/men/30.jpg' }}
+              style={styles.userImage}
+            />
+          </View>
+
+          <View style={styles.searchContainer}>
+            <Input
+              placeholder={'Search...'}
+              value={searchInput}
+              onChangeText={text => setSearchInput(text)}
+              rightIcon
+              leftIcon
+            />
+          </View>
+
+          <ScrollViewH>
+            {categories?.map((item: TCategory) => (
+              <TouchableOpacity
+                key={item.idCategory}
+                onPress={() => {
+                  setSearchCategory(item.strCategory);
+                }}
+                activeOpacity={0.8}
+              >
+                <CatItem
+                  item={item}
+                  selected={searchCategory === item.strCategory}
+                />
+              </TouchableOpacity>
+            ))}
+          </ScrollViewH>
+
+          <View style={styles.headerView}>
+            <H
+              text={'Popular Items'}
+              fontSize={24}
+              style={{ color: colors.black }}
+            />
+            <H
+              text={'See all'}
+              fontSize={16}
+              style={{ opacity: 0.5, color: colors.black }}
+            />
+          </View>
+
+          <ScrollViewH>
+            {categoryItems?.length > 0 && newCategories?.length > 0 ? (
+              newCategories?.map((item: TCategoryItem) => (
+                <TouchableOpacity
+                  key={item.idMeal}
+                  onPress={() => navigation?.navigate('ItemScreen', { item })}
+                  activeOpacity={0.7}
+                >
+                  <MealItem
+                    item={item}
+                    addItemToCart={() => addItemToCart(item)}
+                  />
+                </TouchableOpacity>
+              ))
+            ) : (
+              <View style={styles.loadingView}>
+                <EmptyState searchTerm={searchInput} />
+              </View>
+            )}
+          </ScrollViewH>
+        </ScrollView>
+        <View
+          style={{
+            ...GlobalStyles.wrapper,
+            ...GlobalStyles.lastFloat,
+          }}
+        >
+          <MainCart cart={cart} />
         </View>
-      </ScrollView>
+      </View>
     </Container>
   );
 };
 
 const mapStateToProps = (state: AppState) => {
   return {
+    cart: state.cart.cart,
     categories: state.cart.categories,
     categoryItems: state.cart.categoryItems,
   };
@@ -165,6 +163,7 @@ const mapStateToProps = (state: AppState) => {
 
 const mapDispatchToProps = {
   getCategoryItems: getCategoryItemsDispatcher,
+  addItemToCart: addItemToCartDispatcher,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(MainScreen);
---
+++ app/view/MainScreen/styles.ts
@@ -0,0 +1,33 @@
+import GlobalStyles, { colors } from 'app/design/GlobalStyles';
+import { screenWidth } from 'app/design/responsiveModule';
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  mainScrollView: { marginBottom: 100 },
+  headerView: {
+    ...GlobalStyles.wrapper,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  nameText: { opacity: 0.5, marginBottom: 12, color: colors.black },
+  userImage: {
+    borderRadius: 99,
+    backgroundColor: colors.primaryColor,
+    height: 50,
+    width: 50,
+    alignSelf: 'flex-start',
+  },
+  searchContainer: {
+    ...GlobalStyles.wrapper,
+    marginBottom: 20,
+  },
+  loadingView: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    width: screenWidth - 40,
+    flex: 1,
+  },
+});

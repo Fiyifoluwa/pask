8659248e321c6d9ad803f48e9999921ca1a637a4
Fiyifoluwa <fiyifoluwa@outlook.com>
2022-11-08 10:04:42 +0100

    type definition for all svg files for svg

--- app/definitions.d.ts
+++ app/definitions.d.ts
@@ -22,3 +22,9 @@ interface INavigationProps {
     params: any;
   };
 }
+
+declare module '*.svg' {
+  import { SvgProps } from 'react-native-svg';
+  const content: React.FC<SvgProps>;
+  export default content;
+}
